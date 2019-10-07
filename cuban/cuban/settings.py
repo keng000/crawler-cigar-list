57129e6952e9d93f12b8d161222f9f30f84fe8d1
keng000 <4s.flashback@gmail.com>
2019-10-07 16:06:37 +0900

    lint

--- cuban/cuban/settings.py
+++ cuban/cuban/settings.py
@@ -16,7 +16,7 @@ NEWSPIDER_MODULE = "cuban.spiders"
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.1 Safari/605.1.15'
+USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.1 Safari/605.1.15"
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
--- cuban/cuban/spiders/products.py
+++ cuban/cuban/spiders/products.py
@@ -4,60 +4,63 @@ from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 from w3lib import html
 
+
 class ProductsSpider(CrawlSpider):
-    name = 'products'
-    allowed_domains = ['www.cubancigarwebsite.com']
+    name = "products"
+    allowed_domains = ["www.cubancigarwebsite.com"]
     # start_urls = ['http://www.cubancigarwebsite.com/']
     # start_urls = ['https://www.cubancigarwebsite.com/brands/']
-    start_urls = ['https://www.cubancigarwebsite.com/brand/cohiba',
- 'https://www.cubancigarwebsite.com/brand/h-upmann',
- 'https://www.cubancigarwebsite.com/brand/hoyo-de-monterrey',
- 'https://www.cubancigarwebsite.com/brand/montecristo',
- 'https://www.cubancigarwebsite.com/brand/partagas',
- 'https://www.cubancigarwebsite.com/brand/romeo-y-julieta',
- 'https://www.cubancigarwebsite.com/brand/bolivar',
- 'https://www.cubancigarwebsite.com/brand/punch',
- 'https://www.cubancigarwebsite.com/brand/ramon-allones',
- 'https://www.cubancigarwebsite.com/brand/trinidad',
- 'https://www.cubancigarwebsite.com/brand/jose-l-piedra',
- 'https://www.cubancigarwebsite.com/brand/quintero',
- 'https://www.cubancigarwebsite.com/brand/vegueros',
- 'https://www.cubancigarwebsite.com/brand/cuaba',
- 'https://www.cubancigarwebsite.com/brand/diplomaticos',
- 'https://www.cubancigarwebsite.com/brand/el-rey-del-mundo',
- 'https://www.cubancigarwebsite.com/brand/fonseca',
- 'https://www.cubancigarwebsite.com/brand/juan-lopez',
- 'https://www.cubancigarwebsite.com/brand/la-flor-de-cano',
- 'https://www.cubancigarwebsite.com/brand/la-gloria-cubana',
- 'https://www.cubancigarwebsite.com/brand/por-larranaga',
- 'https://www.cubancigarwebsite.com/brand/quai-dorsay',
- 'https://www.cubancigarwebsite.com/brand/rafael-gonzalez',
- 'https://www.cubancigarwebsite.com/brand/saint-luis-rey',
- 'https://www.cubancigarwebsite.com/brand/san-cristobal',
- 'https://www.cubancigarwebsite.com/brand/sancho-panza',
- 'https://www.cubancigarwebsite.com/brand/vegas-robaina',
- 'https://www.cubancigarwebsite.com/brand/belinda',
- 'https://www.cubancigarwebsite.com/brand/club',
- 'https://www.cubancigarwebsite.com/brand/guantanamera',
- 'https://www.cubancigarwebsite.com/brand/mini',
- 'https://www.cubancigarwebsite.com/brand/puritos',
- 'https://www.cubancigarwebsite.com/brand/troya',
- 'https://www.cubancigarwebsite.com/brand/cubatabaco',
- 'https://www.cubancigarwebsite.com/brand/edmundo-dantes',
- 'https://www.cubancigarwebsite.com/brand/habanos',
- 'https://www.cubancigarwebsite.com/brand/cabanas',
- 'https://www.cubancigarwebsite.com/brand/caney',
- 'https://www.cubancigarwebsite.com/brand/cifuentes',
- 'https://www.cubancigarwebsite.com/brand/davidoff',
- 'https://www.cubancigarwebsite.com/brand/dunhill',
- 'https://www.cubancigarwebsite.com/brand/gispert',
- 'https://www.cubancigarwebsite.com/brand/la-corona',
- 'https://www.cubancigarwebsite.com/brand/la-escepcion',
- 'https://www.cubancigarwebsite.com/brand/la-flor-del-caney',
- 'https://www.cubancigarwebsite.com/brand/los-statos-de-luxe',
- 'https://www.cubancigarwebsite.com/brand/maria-guerrero',
- 'https://www.cubancigarwebsite.com/brand/san-luis-rey',
- 'https://www.cubancigarwebsite.com/brand/siboney']
+    start_urls = [
+        "https://www.cubancigarwebsite.com/brand/cohiba",
+        "https://www.cubancigarwebsite.com/brand/h-upmann",
+        "https://www.cubancigarwebsite.com/brand/hoyo-de-monterrey",
+        "https://www.cubancigarwebsite.com/brand/montecristo",
+        "https://www.cubancigarwebsite.com/brand/partagas",
+        "https://www.cubancigarwebsite.com/brand/romeo-y-julieta",
+        "https://www.cubancigarwebsite.com/brand/bolivar",
+        "https://www.cubancigarwebsite.com/brand/punch",
+        "https://www.cubancigarwebsite.com/brand/ramon-allones",
+        "https://www.cubancigarwebsite.com/brand/trinidad",
+        "https://www.cubancigarwebsite.com/brand/jose-l-piedra",
+        "https://www.cubancigarwebsite.com/brand/quintero",
+        "https://www.cubancigarwebsite.com/brand/vegueros",
+        "https://www.cubancigarwebsite.com/brand/cuaba",
+        "https://www.cubancigarwebsite.com/brand/diplomaticos",
+        "https://www.cubancigarwebsite.com/brand/el-rey-del-mundo",
+        "https://www.cubancigarwebsite.com/brand/fonseca",
+        "https://www.cubancigarwebsite.com/brand/juan-lopez",
+        "https://www.cubancigarwebsite.com/brand/la-flor-de-cano",
+        "https://www.cubancigarwebsite.com/brand/la-gloria-cubana",
+        "https://www.cubancigarwebsite.com/brand/por-larranaga",
+        "https://www.cubancigarwebsite.com/brand/quai-dorsay",
+        "https://www.cubancigarwebsite.com/brand/rafael-gonzalez",
+        "https://www.cubancigarwebsite.com/brand/saint-luis-rey",
+        "https://www.cubancigarwebsite.com/brand/san-cristobal",
+        "https://www.cubancigarwebsite.com/brand/sancho-panza",
+        "https://www.cubancigarwebsite.com/brand/vegas-robaina",
+        "https://www.cubancigarwebsite.com/brand/belinda",
+        "https://www.cubancigarwebsite.com/brand/club",
+        "https://www.cubancigarwebsite.com/brand/guantanamera",
+        "https://www.cubancigarwebsite.com/brand/mini",
+        "https://www.cubancigarwebsite.com/brand/puritos",
+        "https://www.cubancigarwebsite.com/brand/troya",
+        "https://www.cubancigarwebsite.com/brand/cubatabaco",
+        "https://www.cubancigarwebsite.com/brand/edmundo-dantes",
+        "https://www.cubancigarwebsite.com/brand/habanos",
+        "https://www.cubancigarwebsite.com/brand/cabanas",
+        "https://www.cubancigarwebsite.com/brand/caney",
+        "https://www.cubancigarwebsite.com/brand/cifuentes",
+        "https://www.cubancigarwebsite.com/brand/davidoff",
+        "https://www.cubancigarwebsite.com/brand/dunhill",
+        "https://www.cubancigarwebsite.com/brand/gispert",
+        "https://www.cubancigarwebsite.com/brand/la-corona",
+        "https://www.cubancigarwebsite.com/brand/la-escepcion",
+        "https://www.cubancigarwebsite.com/brand/la-flor-del-caney",
+        "https://www.cubancigarwebsite.com/brand/los-statos-de-luxe",
+        "https://www.cubancigarwebsite.com/brand/maria-guerrero",
+        "https://www.cubancigarwebsite.com/brand/san-luis-rey",
+        "https://www.cubancigarwebsite.com/brand/siboney",
+    ]
     # rules = (
     #     Rule(LinkExtractor(allow=r'/brand/.*'), callback='parse_item', follow=True),
     # )
@@ -69,9 +72,9 @@ class ProductsSpider(CrawlSpider):
     def parse(self, response):
         for _i in response.css('div[itemtype="http://schema.org/Product"]'):
             item = {}
-            item['brand'] = _i.css('[itemtype="http://schema.org/Brand"]').css('::text').extract_first().strip()
-            item['name'] = _i.css('.cigarDetailsName::text').extract_first().strip()
-            item['shape'] = html.remove_tags(_i.css('.cigarDetailsCommonName').get()).strip()
+            item["brand"] = _i.css('[itemtype="http://schema.org/Brand"]').css("::text").extract_first().strip()
+            item["name"] = _i.css(".cigarDetailsName::text").extract_first().strip()
+            item["shape"] = html.remove_tags(_i.css(".cigarDetailsCommonName").get()).strip()
             path = _i.css("img::attr('src')").get()
-            item['img'] = f"{response.url}{path}"
+            item["img"] = f"https://www.cubancigarwebsite.com{path}"
             yield item
