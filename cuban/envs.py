7736cd75f105f8bd629a5d9a6b39f072350e721d
keng000 <4s.flashback@gmail.com>
2019-10-13 21:18:50 +0900

    impl blog post func.

--- cuban/envs.py
+++ cuban/envs.py
@@ -1,4 +1,9 @@
 from environs import Env
 from pathlib import Path
+
 env = Env()
 env.read_env(Path(__file__).parent / ".env")
+
+HATENA_USER_NAME = env.str("HATENA_USER_NAME", None)
+HATENA_API_KEY = env.str("HATENA_API_KEY", None)
+HATENA_BLOG_NAME = env.str("HATENA_BLOG_NAME", None)
