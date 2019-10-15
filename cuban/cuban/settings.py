29fced0e6b023e70a63a0cebb618ac9cfdb1fa88
keng000 <4s.flashback@gmail.com>
2019-10-15 21:15:08 +0900

    update params

--- cuban/cuban/settings.py
+++ cuban/cuban/settings.py
@@ -22,7 +22,7 @@ USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/605.1.
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 4
+CONCURRENT_REQUESTS = 2
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
